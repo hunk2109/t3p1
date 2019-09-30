9165939abcf0cd91bbc3254e85475fff8e59e2f3
Hector Jose Taveras Rodriguez <hectojo_2109@hotmail.com>
2019-09-29 22:22:17 -0400

    Agregar archivos de proyecto.

---
+++ nodo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cola cla = new cola();
+            Console.WriteLine("Coloque 5 elementos");
+            cla.encolar(3);
+            cla.encolar(2);
+            cla.encolar(2);
+            cla.encolar(2);            
+            cla.mostrar();
+
+            Console.WriteLine("Retirar 2 elementos");
+            cla.desencolar();
+            cla.mostrar();
+            cla.desencolar();
+            cla.mostrar();
+
+            Console.WriteLine("Retirar un nodo mas de {0}",cla.desencolarvalor());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ nodo/cola.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nodo
+{
+    class cola
+    {
+        public nodo primero;
+        public nodo ultimo;
+
+        public cola()
+        {
+            primero = ultimo = null;
+        }
+
+
+        public void encolar(int valor)
+        {
+            nodo aux = new nodo();
+            aux.info = valor;
+            if(primero == null)
+            {
+                primero = ultimo = aux;
+                aux.sgte = null;
+            }
+
+            else
+            {
+                ultimo.sgte = aux;
+                aux.sgte = null;
+                ultimo = aux;
+            }
+        }
+
+        public void desencolar()
+        {
+            if(primero == null)
+            {
+                Console.WriteLine("Cola Vacia");
+            }
+
+            else
+            {
+                primero = primero.sgte;
+            }
+
+
+            
+        }
+
+        public int desencolarvalor()
+        {
+            int valor = 0;
+            if (primero == null)
+            {
+                Console.WriteLine("Cola Vacia");
+            }
+
+            else
+            {
+                valor = primero.info;
+                primero = primero.sgte;
+            }
+
+            return valor;
+        }
+
+        public void mostrar()
+        {
+            if (primero == null)
+            {
+                Console.WriteLine("Cola Vacia");
+
+            }
+
+            else
+            {
+                nodo puntero;
+                puntero = primero;
+
+                do
+                {
+                    Console.Write("{0}\t", puntero.info);
+                    puntero = puntero.sgte;
+                }
+
+                while (puntero != null);
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+
+}
